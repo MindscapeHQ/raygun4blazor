5f90d065fdbc04917e2117a2c460f17766b298ac
Miguel Beltran <m@beltran.work>
2024-07-16 08:49:50 +0200

    test break formatting

--- src/Raygun.NetCore.Blazor/Models/BrowserSpecs.cs
+++ src/Raygun.NetCore.Blazor/Models/BrowserSpecs.cs
@@ -11,7 +11,11 @@ namespace Raygun.NetCore.Blazor.Models
     /// This exists because we need to parse some stuff from the browser to construct a proper <see cref="EnvironmentDetails" />
     /// instance. Also the <see cref="EnvironmentDetails" /> is a hot mess of JavaScript serialization inconsistency.
     /// </remarks>
-    internal record BrowserSpecs
+    internal record 
+        
+        
+        
+        BrowserSpecs
     {
 
         #region Private Members
