144d1308bd9f01e5cfd6acf3dfcd5ccfa418c5aa
Miguel Beltran <m@beltran.work>
2024-09-13 11:39:49 +0200

    WIP Adding MAUI Extension

---
+++ src/Raygun.Blazor.Maui/MauiWebExtensions.cs
@@ -0,0 +1,39 @@
+﻿using KristofferStrube.Blazor.Window;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Raygun.Blazor.Interfaces;
+using Raygun.Blazor.Offline;
+using Raygun.Blazor.Offline.SendStrategy;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Raygun.Blazor.Maui.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class MauiWebExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void UseRaygunBlazor(this MauiAppBuilder builder, string configSectionName = "Raygun")
+        {
+            builder.Services.Configure<RaygunSettings>(builder.Configuration.GetSection(configSectionName));
+            builder.Services.AddScoped<RaygunBrowserInterop>();
+            builder.Services.AddScoped<IWindowService, WindowService>();
+            builder.Services.AddScoped<IBackgroundSendStrategy, TimerBasedSendStrategy>();
+            builder.Services.AddScoped<IRaygunOfflineStore, RaygunLocalOfflineStore>();
+
+            builder.Services.AddHttpClient("Raygun")
+                .ConfigureHttpClient((sp, client) =>
+                {
+                    var raygunSettings = sp.GetRequiredService<IOptions<RaygunSettings>>().Value;
+                    client.BaseAddress = new Uri(raygunSettings.Endpoint);
+                    client.DefaultRequestHeaders.Add("X-ApiKey", raygunSettings.ApiKey);
+                });
+
+            builder.Services.AddScoped<RaygunBlazorClient>();
+        }
+    }
+}
--- src/Raygun.Samples.Blazor.MauiHybrid/MauiProgram.cs
+++ src/Raygun.Samples.Blazor.MauiHybrid/MauiProgram.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Raygun.Blazor;
+using Raygun.Blazor.Maui.Extensions;
+using System.Reflection;
 
 namespace Raygun.Samples.Blazor.MauiHybrid
 {
@@ -6,19 +10,26 @@ namespace Raygun.Samples.Blazor.MauiHybrid
     {
         public static MauiApp CreateMauiApp()
         {
+            // Add Raygun configuration
+            var a = Assembly.GetExecutingAssembly();
+            using var stream = a.GetManifestResourceStream("appsettings.json");
+
+            // MAUI App should be configured using Raygun4MAUI
             var builder = MauiApp.CreateBuilder();
+            builder.Configuration.AddJsonStream(stream!);
             builder
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+                })
+                .UseRaygunBlazor();
 
             builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
