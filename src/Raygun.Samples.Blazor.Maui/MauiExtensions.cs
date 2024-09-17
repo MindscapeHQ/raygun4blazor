e4912b0cd8fab6348fcadb8d42ae65fd9fc5e2ee
Miguel Beltran <m@beltran.work>
2024-09-17 11:50:05 +0200

    import dependency and fill in the extension function

--- src/Raygun.Samples.Blazor.Maui/MauiExtensions.cs
+++ src/Raygun.Samples.Blazor.Maui/MauiExtensions.cs
@@ -1,15 +1,37 @@
-﻿using System;
+﻿using KristofferStrube.Blazor.Window;
+using Microsoft.Extensions.Options;
+using Raygun.Blazor;
+using Raygun.Blazor.Interfaces;
+using Raygun.Blazor.Offline;
+using Raygun.Blazor.Offline.SendStrategy;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// TODO: Move to package
 namespace Raygun.Samples.Blazor.Maui
 {
     public static class MauiExtensions
     {
-        public static MauiAppBuilder UseRaygunBlazorMaui(this MauiAppBuilder builder)
+        public static MauiAppBuilder UseRaygunBlazorMaui(this MauiAppBuilder builder, string configSectionName = "Raygun")
         {
+            builder.Services.Configure<RaygunSettings>(builder.Configuration.GetSection(configSectionName));
+            builder.Services.AddScoped<RaygunBrowserInterop>();
+            builder.Services.AddScoped<IWindowService, WindowService>();
+            builder.Services.AddScoped<IBackgroundSendStrategy, TimerBasedSendStrategy>();
+            builder.Services.AddScoped<IRaygunOfflineStore, RaygunLocalOfflineStore>();
+
+            builder.Services.AddHttpClient("Raygun")
+                .ConfigureHttpClient((sp, client) =>
+                {
+                    var raygunSettings = sp.GetRequiredService<IOptions<RaygunSettings>>().Value;
+                    client.BaseAddress = new Uri(raygunSettings.Endpoint);
+                    client.DefaultRequestHeaders.Add("X-ApiKey", raygunSettings.ApiKey);
+                });
+
+            builder.Services.AddScoped<RaygunBlazorClient>();
             return builder;
         }
     }
