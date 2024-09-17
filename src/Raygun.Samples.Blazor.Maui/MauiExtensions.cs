018fa725e567bcbeb24203ac4908b3b56618b0a7
Miguel Beltran <m@beltran.work>
2024-09-17 11:46:31 +0200

    Created Maui sample with an internal extension function

---
+++ src/Raygun.Samples.Blazor.Maui/MauiExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raygun.Samples.Blazor.Maui
+{
+    public static class MauiExtensions
+    {
+        public static MauiAppBuilder UseRaygunBlazorMaui(this MauiAppBuilder builder)
+        {
+            return builder;
+        }
+    }
+}
