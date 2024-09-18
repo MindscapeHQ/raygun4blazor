3367f2e8a7da7af1e52d1a6da45344f2c5844698
Miguel Beltran <m@beltran.work>
2024-09-18 09:49:32 +0200

    created empty Blazor Maui lib project

---
+++ src/Raygun.Blazor.Maui/Platforms/Android/PlatformClass1.cs
@@ -0,0 +1,6 @@
+﻿namespace Raygun.Blazor.Maui;
+
+// All the code in this file is only included on Android.
+public class PlatformClass1
+{
+}
---
+++ src/Raygun.Blazor.Maui/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Raygun.Blazor.Maui
+{
+	// All the code in this file is only included on Tizen.
+	public class PlatformClass1
+	{
+	}
+}
